35e1530f975246ced8a82c62217c952f32c8e1a1
Sergey Ivanov <sergeykonstwork@gmail.com>
2025-06-01 17:08:34 +0300

    Поменял compose файл и обновил приложение + mongoDB

--- practice_3/app.py
+++ practice_3/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+# Подключение к MongoDB через переменную среды
+mongo_uri = os.environ.get("MONGO_URI", "mongodb://localhost:27017/mydb")
+client = MongoClient(mongo_uri)
+db = client.get_database()
 
 @app.route('/')
 def home():
-    return 'Hello, Docker!'
-
+    return 'Hello, Docker & MongoDB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
